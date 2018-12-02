6f86ce4665040d15fd1fc67a4e763a9367eac9b0
DanielBaas <jn15_1617@hotmail.com>
2018-12-02 12:57:19 -0600

    Cambios realizados
    Elimine los archivos index que estaban fuera y el archivo de jquery.
    
    Agregue una carpeta llamada MinimizedVersionRSS que contiene los archivos html, js y php ya minimizados.

--- extraerBD.php
+++ extraerBD.php
@@ -12,33 +12,13 @@ $password = "";
     if( ! $mysqli ) die( "Error de conexion ".mysqli_connect_error() );
  
 
-// Esta sentencia usa LIKE
-
-/* $sql = "SELECT * FROM noticias WHERE 
-    id_noticia LIKE '%$search%' OR
-    link LIKE '%$search%' OR
-    titulo LIKE '%$search%' OR
-    descripcion LIKE '%$search%' OR
-    fecha LIKE '%$search%'"; */
-
-
-// Esta sentencia usa MATCH
-// Antes de ejecutar la sentencia hay que ejecutar lo siguiente:
-// 
-// alter table noticias ADD FULLTEXT INDEX Buscar (titulo ASC, descripcion ASC)
 $sql = "SELECT * FROM noticias WHERE MATCH(titulo,descripcion) against('$search')";
 
 $result = mysqli_query($mysqli, $sql);
 
-/*
-while($obj = mysqli_fetch_assoc($result)) {
-$var[] = $obj;
-
-}*/
-//echo $result->num_rows;
 $array = array();
 if ($result->num_rows > 0) {
-    // output data of each row
+
     while($row = $result->fetch_assoc()) {
         $array[] = $row;
     }
